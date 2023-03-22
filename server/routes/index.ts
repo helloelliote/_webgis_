847e7a3446484f0785973d8c0f2550592c194e88
helloelliote <hello.elliote@gmail.com>
2023-03-22 14:17:54 +0900

    fix: create-react-app 에서 잘못 생성된 manifest.json

--- server/routes/index.ts
+++ server/routes/index.ts
@@ -1,11 +1,20 @@
 import { NextFunction, Request, Response, Router } from "express";
+import { readFileSync } from "fs";
 import { resolve } from "path";
 import geoserver from "../middlewares/geoserver";
 
 const router: Router = Router();
 
-router.get("/", (req: Request, res: Response, next: NextFunction) => {
-  res.sendFile("index.html", { root: resolve(__dirname, "public") });
+router.get("/", async (req: Request, res: Response, next: NextFunction) => {
+  try {
+    const html = readFileSync(
+      resolve(resolve(__dirname, "public"), "index.html"),
+      "utf-8"
+    );
+    res.send(html);
+  } catch (e) {
+    return next(e);
+  }
 });
 
 router.get(/geoserver/, geoserver);
